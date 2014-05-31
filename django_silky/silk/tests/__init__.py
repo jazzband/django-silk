6b8a41009461edca2e15b140b5d01a026e528e05
Michael Ford <mtford@gmail.com>
2014-05-31 19:46:05 +0100

    Build fail for py3 due to __init__ weirdness

--- django_silky/silk/tests/__init__.py
+++ django_silky/silk/tests/__init__.py
@@ -1,9 +1,9 @@
-from test_code_gen_curl import *
-from test_collector import *
-from test_dynamic_profiling import *
-from test_execute_sql import *
-from test_silky_middleware import *
-from test_silky_profiler import *
-from test_view_profiling import *
-from test_view_requests import *
+from silk.tests.test_code_gen_curl import *
+from silk.tests.test_collector import *
+from silk.tests.test_dynamic_profiling import *
+from silk.tests.test_execute_sql import *
+from silk.tests.test_silky_middleware import *
+from silk.tests.test_silky_profiler import *
+from silk.tests.test_view_profiling import *
+from silk.tests.test_view_requests import *
 
