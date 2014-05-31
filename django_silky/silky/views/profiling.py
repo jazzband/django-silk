7025cf4bea284e53595bf003030b2aefc9f5c14a
Michael Ford <mtford@gmail.com>
2014-05-31 17:16:35 +0100

    Added dependencies to packaging.

--- django_silky/silky/views/profiling.py
+++ django_silky/silky/views/profiling.py
@@ -1,19 +1,4 @@
-# def profiling(request, request_id):
-#     r = Request.objects.get(pk=request_id)
-#     query_set = Profile.objects.filter(request=r).order_by('-start_time')
-#     page = _page(request, query_set)
-#     return render_to_response('silky/profiling.html', {
-#         'request': r,
-#         'profiles': page
-#     })
-#
-#
-# def profile(request, profile_id):
-#     profile = Profile.objects.get(pk=profile_id)
-#     context = {'profile': profile}
-#     if profile.file_path and profile.line_num:
-#         context['rendered_code'] = _code_context(profile.file_path, profile.line_num)
-#     return render_to_response('silky/profile.html', context)
+
 from django.shortcuts import render_to_response
 from django.views.generic import View
 
--- package.py
+++ package.py
@@ -29,14 +29,14 @@ def main():
         manifest_file_name = '/MANIFEST.in'
         for file_name in readme_file_name, license_file_name, manifest_file_name:
             shutil.copyfile(SCRIPT_DIR + file_name, dist_dir + file_name)
-        silky_dir = dist_dir + '/silky/'
+        silky_dir = dist_dir + '/silk/'
         if os.path.exists(silky_dir):
             shutil.rmtree(silky_dir)
         shutil.copytree(SCRIPT_DIR + '/django_silky/silky/', silky_dir)
         cmd = 'cd %s && python setup.py sdist' % dist_dir
         print(cmd)
         subprocess.call(cmd, shell=True)
-        archive_name = 'django-silky-%s.tar.gz' % version
+        archive_name = 'django-silk-%s.tar.gz' % version
         from_file = dist_dir + ('/dist/' + archive_name)
         to_file = dist_dir + '/../' + archive_name
         print('Copying from %s to %s' % (from_file, to_file))
