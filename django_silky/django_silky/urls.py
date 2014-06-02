7d87e398d511f14ac5447730c042df02d1522ac9
Michael Ford <mtford@gmail.com>
2014-06-02 16:28:32 +0100

    Fixed urls

--- django_silky/django_silky/urls.py
+++ django_silky/django_silky/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-                       url(r'^silky', include('silky.urls'))
+                       url(r'^silk', include('silk.urls', namespace='silk'))
                        )
 
