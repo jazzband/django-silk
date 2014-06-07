670e95ce5045aef4a5d453c2231f94274f378e5c
Michael Ford <mtford@gmail.com>
2014-06-07 11:53:42 +0100

    Added setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-silk',
-    version='{{ version }}',
+    version='mas',
     packages=['silk'],
     include_package_data=True,
     license='MIT License',
