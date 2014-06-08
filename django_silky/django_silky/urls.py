0548ced364e8fc12faf4bbfd6acc0652f24bf5d9
Michael Ford <mtford@gmail.com>
2014-06-08 19:44:09 +0100

    Attempts to fix the weird django 1.5 travis build failures

--- django_silky/django_silky/urls.py
+++ django_silky/django_silky/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^silk', include('silk.urls', namespace='silk')),
+                       url(r'^silk', include('silk.urls', namespace='silk', app_name='silk')),
                        url(r'^example_app', include('example_app.urls')),
                        url(r'^admin/', include(admin.site.urls)),
 
