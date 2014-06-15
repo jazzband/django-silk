0989797201aee92cd2cfd6a751c485f069dd62e4
Michael Ford <mtford@gmail.com>
2014-06-15 18:26:56 +0100

    Fixed broken 2.7 compat

--- silk/views/profiling.py
+++ silk/views/profiling.py
@@ -23,7 +23,7 @@ class ProfilingView(View):
     session_key_profile_filters = 'session_key_profile_filters'
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super(ProfilingView, self).__init__(**kwargs)
 
     def _get_distinct_values(self, field, silk_request):
         if silk_request:
