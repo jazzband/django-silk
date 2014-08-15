d29eded722b916014670623a5847b9e4572d9c4d
Joshua Tauberer <jt@occams.info>
2014-08-14 20:24:46 -0400

    have silk_clear_request_log clear the dependent tables first to make it easier on Django to check consistency

--- silk/management/commands/silk_clear_request_log.py
+++ silk/management/commands/silk_clear_request_log.py
@@ -4,8 +4,17 @@ import silk.models
 
 class Command(BaseCommand):
 	help = "Clears silk's log of requests."
+
+	@staticmethod
+	def delete_model(model):
+		count = model.objects.count()
+		print("deleting %s %s objects" % (model.__name__, count))
+		model.objects.all().delete()
 	
 	def handle(self, *args, **options):
-		count = silk.models.Request.objects.count()
-		print("deleting %s request objects" % count)
-		silk.models.Request.objects.all().delete()
+		# Django takes a long time to traverse foreign key relations,
+		# so delete in the order that makes it easy.
+		Command.delete_model(silk.models.Profile)
+		Command.delete_model(silk.models.SQLQuery)
+		Command.delete_model(silk.models.Response)
+		Command.delete_model(silk.models.Request)
