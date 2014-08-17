83f4556ec882e41d55210749aa7e260cb937580d
Michael Ford <mtford@gmail.com>
2014-08-17 21:44:31 +0100

    Remove south from apps so build will finish

--- django_silky/django_silky/settings.py
+++ django_silky/django_silky/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.sessions',
-    'south',
+    # 'south',
     'silk',
     'example_app'
 )
