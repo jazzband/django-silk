8d0e59737aea1caa4a6020dcf9a26c24c129b2ba
Michael Ford <mtford@gmail.com>
2014-09-10 09:14:08 +0100

    Attempts to fix

--- silk/sql.py
+++ silk/sql.py
@@ -8,7 +8,6 @@ from django.db.backends.util import CursorWrapper
 from silk.collector import DataCollector
 from silk.config import SilkyConfig
 
-
 Logger = logging.getLogger('silk')
 
 
@@ -23,19 +22,32 @@ def wrap_cursor_execute():
     if getattr(connection.cursor, '__silk__wrapped', False):
         return
     original_get_cursor = connection.cursor
+
     def wrapped_get_cursor():
         cursor = original_get_cursor()
         return SilkyCursorWrapper(cursor, connection)
+
     wrapped_get_cursor.__silk__wrapped = True
     connection.cursor = wrapped_get_cursor
 
 
 class SilkyCursorWrapper(CursorWrapper):
+    def _should_wrap(self, sql, params):
+        """
+        :param sql:
+        :param params:
+        :return: String representation of sql query if should wrap, else false
+        """
+        sql_query = sql % params
+        if _should_wrap(sql_query):
+            return sql_query
+        return False
+
     def execute(self, sql, params=()):
-        tb = ''.join(reversed(traceback.format_stack()))
         sql_query = sql % params
         if not _should_wrap(sql_query):
             return self.cursor.execute(sql, params)
+        tb = ''.join(reversed(traceback.format_stack()))
         query_dict = {
             'query': sql_query,
             'start_time': timezone.now(),
@@ -48,5 +60,21 @@ class SilkyCursorWrapper(CursorWrapper):
             DataCollector().register_query(query_dict)
 
     def executemany(self, sql, param_list):
-        # TODO
-        return self.cursor.executemany(sql, param_list)
+        sql_queries = [self._should_wrap(sql, params) for params in param_list]
+        if not all(sql_queries):
+            return self.cursor.executemany(sql, param_list)
+        tb = ''.join(reversed(traceback.format_stack()))
+        start_time = timezone.now()
+        try:
+            return self.cursor.executemany(sql, param_list)
+        finally:
+            for sql_query in sql_queries:
+                # TODO: We can improve this by somehow finding the start/end time of each individual query.
+                # TODO: Alternatively, we could add a ManySQLQuery model so we can at least acknowledge the fact.
+                query_dict = {
+                    'query': sql_query,
+                    'start_time': start_time,
+                    'end_time': timezone.now(),
+                    'traceback': tb
+                }
+                DataCollector().register_query(query_dict)
--- silk/tests/test_execute_sql.py
+++ silk/tests/test_execute_sql.py
@@ -3,8 +3,9 @@ from mock import Mock, NonCallableMock, NonCallableMagicMock, patch
 
 from silk.collector import DataCollector
 from silk.models import SQLQuery, Request
-from silk.sql import execute_sql
 from silk.tests.util import delete_all_models
+
+from silk.sql import SilkyCursorWrapper
 
 
 def mock_sql():
@@ -12,9 +13,13 @@ def mock_sql():
     mock_sql_query._execute_sql = Mock()
     mock_sql_query.query = NonCallableMock(spec_set=['model'])
     mock_sql_query.query.model = Mock()
-    query_string = 'SELECT * from table_name'
+    query_string = 'SELECT * FROM table_name'
     mock_sql_query.as_sql = Mock(return_value=(query_string, ()))
     return mock_sql_query, query_string
+
+
+def mock_cursor():
+    return Mock(spec_set=['execute', 'executemany'])
 
 
 class TestCall(TestCase):
@@ -22,17 +27,16 @@ class TestCall(TestCase):
     def setUpClass(cls):
         DataCollector().configure(request=None)
         delete_all_models(SQLQuery)
-        cls.mock_sql, cls.query_string = mock_sql()
-        kwargs = {
-            'one': 1,
-            'two': 2
-        }
-        cls.args = [1, 2]
-        cls.kwargs = kwargs
-        execute_sql(cls.mock_sql, *cls.args, **cls.kwargs)
+        cls.mock_cursor = mock_cursor()
+        cls.mock_db = Mock(spec_set=[])
+        cls.wrapper = SilkyCursorWrapper(cls.mock_cursor, cls.mock_db)
+        cls.params = ('param1', 'param2')
+        cls.query_string = 'SELECT %s FROM %s'
+        cls.processed_query_string = cls.query_string % cls.params
+        cls.wrapper.execute(cls.query_string, cls.params)
 
     def test_called(self):
-        self.mock_sql._execute_sql.assert_called_once_with(*self.args, **self.kwargs)
+        self.mock_cursor.execute.assert_called_once_with(self.query_string, self.params)
 
     def test_count(self):
         self.assertEqual(1, len(DataCollector().queries))
@@ -47,22 +51,28 @@ class TestCall(TestCase):
 
     def test_query(self):
         query = self._get_query()
-        self.assertEqual(query['query'], self.query_string)
+        self.assertEqual(query['query'], self.processed_query_string)
 
 
 class TestCallSilky(TestCase):
     def test_no_effect(self):
         DataCollector().configure()
-        sql, _ = mock_sql()
+        sql, query_string = mock_sql()
         sql.query.model = NonCallableMagicMock(spec_set=['__module__'])
         sql.query.model.__module__ = 'silk.models'
+        cursor = mock_cursor()
+        wrapper = SilkyCursorWrapper(cursor, Mock(spec_set=[]))
         # No SQLQuery models should be created for silk requests for obvious reasons
         with patch('silk.sql.DataCollector', return_value=Mock()) as mock_DataCollector:
-            execute_sql(sql)
+            wrapper.execute(query_string)
             self.assertFalse(mock_DataCollector().register_query.call_count)
 
 
 class TestCollectorInteraction(TestCase):
+
+    def setUp(self):
+        DataCollector().request = Mock()
+
     def _query(self):
         try:
             query = list(DataCollector().queries.values())[0]
@@ -72,15 +82,20 @@ class TestCollectorInteraction(TestCase):
 
     def test_request(self):
         DataCollector().configure(request=Request.objects.create(path='/path/to/somewhere'))
-        sql, _ = mock_sql()
-        execute_sql(sql)
+        sql, qs = mock_sql()
+        cursor = mock_cursor()
+        wrapper = SilkyCursorWrapper(cursor, Mock(spec_set=[]))
+        wrapper.execute(qs)
         query = self._query()
+        print query
         self.assertEqual(query['request'], DataCollector().request)
 
     def test_registration(self):
         DataCollector().configure(request=Request.objects.create(path='/path/to/somewhere'))
-        sql, _ = mock_sql()
-        execute_sql(sql)
+        sql, qs = mock_sql()
+        cursor = mock_cursor()
+        wrapper = SilkyCursorWrapper(cursor, Mock(spec_set=[]))
+        wrapper.execute(qs)
         query = self._query()
         self.assertIn(query, DataCollector().queries.values())
 
