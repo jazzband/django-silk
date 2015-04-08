06072cc7a8584e263143f5fe6265bb07e9d5e7ea
Michael Ford <mtford@gmail.com>
2015-04-08 18:41:49 +0100

    partial 1.8 support (some tests still fail for unknown reasons

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -1,14 +1,1 @@
-"""
-WSGI config for django_silky project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+../wsgi.py
