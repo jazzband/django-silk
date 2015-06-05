deafdcab22aab106e6b08f4b1349a78be5e4f055
Michael Ford <mtford@gmail.com>
2015-06-05 15:42:47 +0100

    js compilation workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,57 @@
-/**
- * Used for automating various tasks around testing & compilation.
- */
-(function () {
-    "use strict";
-
-    var gulp = require('gulp'),
-        plugins = require('gulp-load-plugins')();
+var gulp = require('gulp'),
+    async = require('async'),
+    _ = require('underscore'),
+    plugins = require('gulp-load-plugins')();
 
 
-    gulp.task('watch', function () {
-        gulp.watch('scss/**/*.scss', ['scss']);
-    });
+var webpack = {
+  externals: {
+    'react': 'React',
+    'jQuery': '$',
+    'underscore': '_',
+    'moment': 'moment',
+    'async': 'async'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
 
-    gulp.task('scss', function () {
-        gulp.src('scss/**/*.scss')
-            .pipe(plugins.scss())
-            .pipe(gulp.dest('silk/static/silk/css'))
-    });
 
-})();
+gulp.task('watch', function () {
+  gulp.watch('scss/**/*.scss', ['scss']);
+});
+
+gulp.task('scss', function () {
+  gulp.src('scss/**/*.scss')
+      .pipe(plugins.scss())
+      .pipe(gulp.dest('silk/static/silk/css'))
+});
+
+var apps = {
+  summary: 'js/summary/index.js'
+};
+
+gulp.task('js', function (done) {
+  var appNames = Object.keys(apps),
+      tasks = _.map(appNames, function (appName) {
+        return function (done) {
+          var path = apps[appName],
+              config = _.extend({}, webpack);
+          gulp.src(path)
+              .pipe(plugins.webpack(config))
+              .pipe(plugins.rename(appName + '.js'))
+              .pipe(gulp.dest('silk/static/silk/js'))
+              .on('end', done);
+        }
+      });
+  async.parallel(tasks, done);
+});
+
---
+++ js/summary/index.js
@@ -0,0 +1,2 @@
+var React = require('react');
+
