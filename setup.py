7e4bedb734d009365a94537124979d13c2b836d4
Michael Ford <mtford@gmail.com>
2015-09-06 08:24:52 +0100

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-silk',
-    version='0.5.5',
+    version='0.5.6',
     packages=['silk'],
     include_package_data=True,
     license='MIT License',
