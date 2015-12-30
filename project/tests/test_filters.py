cd4e8aadcd007c1d79be491b2e735d14647afe21
Andrew Velis <andrew.velis@gmail.com>
2015-12-30 02:04:42 -0600

    Optimization of imports in test_filters class.
    This increases readability and better organizes imports by utilizing
    best practices.

--- project/tests/test_filters.py
+++ project/tests/test_filters.py
@@ -1,18 +1,27 @@
 import calendar
+import random
 from datetime import timedelta, datetime
 from math import floor
-import random
 
-from django.utils import timezone
-from django.test import TestCase
 import pytz
+from django.test import TestCase
+from django.utils import timezone
 
 from silk import models
-from silk.request_filters import SecondsFilter, AfterDateFilter, BeforeDateFilter, ViewNameFilter, PathFilter, NameFilter, FunctionNameFilter, NumQueriesFilter, TimeSpentOnQueriesFilter, \
-    OverallTimeFilter
+from silk.request_filters import (
+    SecondsFilter,
+    AfterDateFilter,
+    BeforeDateFilter,
+    ViewNameFilter,
+    PathFilter,
+    NameFilter,
+    FunctionNameFilter,
+    NumQueriesFilter,
+    TimeSpentOnQueriesFilter,
+    OverallTimeFilter,
+)
 from .test_lib.mock_suite import MockSuite
 from .util import delete_all_models
-
 
 mock_suite = MockSuite()
 
