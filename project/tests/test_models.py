eb899bc02f17a2a3462aa8668e2a42302f776a1b
Alexey Kalinin <lyoffa412@gmail.com>
2016-01-13 20:04:15 +0500

    prepered for models testing

---
+++ project/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+from silk import models
+
+
+class RequestTest(TestCase):
+    pass
+
+
+class ResponseTest(TestCase):
+    pass
+
+
+class SQLQueryManagerTest(TestCase):
+    pass
+
+
+class SQLQueryTest(TestCase):
+    pass
+
+
+class BaseProfileTest(TestCase):
+    pass
+
+
+class ProfileTest(TestCase):
+    pass
