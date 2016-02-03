ab68444e76598cf1b91c587e06ce31409c4a0e75
Drew Hubl <drew.hubl+github@gmail.com>
2016-02-03 00:48:39 -0700

    Django 1.9 compatibility for the csrf context processor

--- silk/views/profiling.py
+++ silk/views/profiling.py
@@ -1,4 +1,4 @@
-from django.core.context_processors import csrf
+from django.template.context_processors import csrf
 from django.db.models import Count, Sum
 from django.shortcuts import render_to_response
 from django.utils.decorators import method_decorator
