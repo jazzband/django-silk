d31443fffb7f08b530a1c759d4ea65f877d1df5e
Hanley Hansen <hanleyhansen@gmail.com>
2016-07-08 11:25:10 -0400

    bring back delete_all_models() for sqlite's sake

--- project/tests/util.py
+++ project/tests/util.py
@@ -10,3 +10,13 @@ def mock_data_collector():
     mock.local.request = r
     mock.request = r
     return mock
+
+def delete_all_models(model_class):
+    """
+    A sqlite3-safe deletion function to avoid "django.db.utils.OperationalError: too many SQL variables"
+    :param model_class:
+    :return:
+    """
+    while model_class.objects.count():
+        ids = model_class.objects.values_list('pk', flat=True)[:80]
+        model_class.objects.filter(pk__in=ids).delete()
