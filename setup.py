cb0a4fd5fc9b730f48af7c185a54782698b900c6
Andrew Velis <avelis@users.noreply.github.com>
2016-07-13 12:51:31 -0500

    Bumps package version for maintenance release
    This change prepares this package for the latest fix in regards to an install issue in the this file.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-silk',
-    version='0.6.0',
+    version='0.6.1',
     packages=['silk'],
     include_package_data=True,
     license='MIT License',
