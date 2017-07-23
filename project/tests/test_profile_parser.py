3b75393fce7e7d0448da51ded144ee6d734b035a
Daniel Bradburn <moagstar@gmail.com>
2017-07-23 13:09:30 +0200

    fixed failing test due to PY3/2 differences in profile output

--- project/tests/test_profile_parser.py
+++ project/tests/test_profile_parser.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import cProfile
 # 3rd party
 import contextlib2 as contextlib
-from six import StringIO
+from six import StringIO, PY3
 from django.test import TestCase
 # silk
 from silk.utils.profile_parser import parse_profile
@@ -21,11 +21,24 @@ class ProfileParserTestCase(TestCase):
                 cProfile.run('print()')
             stream.seek(0)
             actual = list(parse_profile(stream))
-            expected = [
-                 ['ncalls', 'tottime', 'percall', 'cumtime', 'percall', 'filename:lineno(function)'],
-                 ['1', '0.000', '0.000', '0.000', '0.000', '<string>:1(<module>)'],
-                 ['1', '0.000', '0.000', '0.000', '0.000', '{built-in method builtins.exec}'],
-                 ['1', '0.000', '0.000', '0.000', '0.000', '{built-in method builtins.print}'],
-                 ['1', '0.000', '0.000', '0.000', '0.000', "{method 'disable' of '_lsprof.Profiler' objects}"],
-            ]
+            if PY3:
+                expected = [
+                     ['ncalls', 'tottime', 'percall', 'cumtime', 'percall', 'filename:lineno(function)'],
+                     ['1', '0.000', '0.000', '0.000', '0.000', '<string>:1(<module>)'],
+                     ['1', '0.000', '0.000', '0.000', '0.000', '{built-in method builtins.exec}'],
+                     ['1', '0.000', '0.000', '0.000', '0.000', '{built-in method builtins.print}'],
+                     ['1', '0.000', '0.000', '0.000', '0.000', "{method 'disable' of '_lsprof.Profiler' objects}"],
+                ]
+            else:
+                expected = [
+                    ['ncalls', 'tottime', 'percall', 'cumtime', 'percall', 'filename:lineno(function)'],
+                    ['1', '0.000', '0.000', '0.000', '0.000', '<string>:1(<module>)'],
+                    ['2', '0.000', '0.000', '0.000', '0.000', 'StringIO.py:208(write)'],
+                    ['2', '0.000', '0.000', '0.000', '0.000', 'StringIO.py:38(_complain_ifclosed)'],
+                    ['2', '0.000', '0.000', '0.000', '0.000', '{isinstance}'],
+                    ['2', '0.000', '0.000', '0.000', '0.000', '{len}'],
+                    ['2', '0.000', '0.000', '0.000', '0.000', "{method 'append' of 'list' objects}"],
+                    ['1', '0.000', '0.000', '0.000', '0.000', "{method 'disable' of '_lsprof.Profiler' objects}"]
+                ]
+
             self.assertListEqual(actual, expected)
