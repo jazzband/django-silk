d27247b37fba61b33ea09342ce7e37c2f84e3994
danielbradburn <daniel@crunchrapps.com>
2017-07-24 03:46:25 +0200

    #33 organise cprofile output as a sortable table (#200)
    * add .venv* to .gitignore
    
    * made the profile output a sortable table with links to the appropriate source code
    
    * remove memoization of get_dot function since this was causing problem on python 2, and should probably be in a separate PR
    
    * removed python 3 only spitlines
    
    * fixed failing test due to PY3/2 differences in profile output
    
    * fixed failing test due difference in python 3.4
    
    * fixed failing test due to floating point precision
    
    * fixed problem due to trying to import abs from math
    
    * reverted incorrect attempt to fix flaky test due to floating point precision

--- silk/views/profile_detail.py
+++ silk/views/profile_detail.py
@@ -3,7 +3,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import View
 from silk.auth import login_possibly_required, permissions_possibly_required
 from silk.models import Profile
-from silk.views.sql_detail import _code
+from silk.views.code import _code_context, _code_context_from_request
 
 
 class ProfilingDetailView(View):
@@ -18,16 +18,21 @@ class ProfilingDetailView(View):
         profile = Profile.objects.get(pk=profile_id)
         file_path = profile.file_path
         line_num = profile.line_num
+
+        context['pos'] = pos = int(request.GET.get('pos', 0))
+        if pos:
+            context.update(_code_context_from_request(request, prefix='pyprofile_'))
+
         context['profile'] = profile
         context['line_num'] = file_path
         context['file_path'] = line_num
+        context['file_column'] = 5
+
         if profile.request:
             context['silk_request'] = profile.request
         if file_path and line_num:
             try:
-                actual_line, code = _code(file_path, line_num, profile.end_line_num)
-                context['code'] = code
-                context['actual_line'] = actual_line
+                context.update(_code_context(file_path, line_num, profile.end_line_num))
             except IOError as e:
                 if e.errno == 2:
                     context['code_error'] = e.filename + ' does not exist.'
