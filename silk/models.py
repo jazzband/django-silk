b42d67000b7722105e6eb5754a96a6b1fde2536b
shannon <smcoll@gmail.com>
2017-08-01 09:41:05 -0500

    didn't mean to reorder imports

--- silk/models.py
+++ silk/models.py
@@ -1,24 +1,24 @@
-import base64
+from collections import Counter
 import json
+import base64
 import random
 import re
-from collections import Counter
-from uuid import uuid4
 
-import sqlparse
 from django.core.files.storage import get_storage_class
 from django.db import models
-from django.db import transaction
 from django.db.models import (
     DateTimeField, TextField, CharField, ForeignKey, IntegerField,
     BooleanField, F, ManyToManyField, OneToOneField, FloatField,
     FileField
 )
 from django.utils import timezone
+from django.db import transaction
+from uuid import uuid4
+import sqlparse
 from django.utils.safestring import mark_safe
 
-from silk.config import SilkyConfig
 from silk.utils.profile_parser import parse_profile
+from silk.config import SilkyConfig
 
 # Django 1.8 removes commit_on_success, django 1.5 does not have atomic
 atomic = getattr(transaction, 'atomic', None) or getattr(transaction, 'commit_on_success')
