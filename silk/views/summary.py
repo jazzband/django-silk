56e085f5deff1aef624c0f1cc8761ff82c83702e
Albert Wang <git@albertyw.com>
2017-12-09 16:00:14 -0800

    Fix missing db_time field (#234)

--- silk/views/summary.py
+++ silk/views/summary.py
@@ -39,7 +39,7 @@ class SummaryView(View):
         return requests
 
     def _time_spent_in_db_by_view(self, filters):
-        values_list = models.Request.objects.filter(*filters).values_list('view_name').annotate(t=Sum('queries__time_taken')).filter(db_time__gte=0).order_by('-t')[:5]
+        values_list = models.Request.objects.filter(*filters).values_list('view_name').annotate(t=Sum('queries__time_taken')).filter(t__gte=0).order_by('-t')[:5]
         requests = []
         for view, _ in values_list:
             r = models.Request.objects.filter(view_name=view, *filters).annotate(t=Sum('queries__time_taken')).order_by('-t')[0]
