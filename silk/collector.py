a446c574a87a79e5a2f12043bcdf662f3ad72098
Carlo Vallerga <carlo.vallerga@gmail.com>
2018-10-03 12:16:52 +0200

    Improve file name for generated profile file

--- silk/collector.py
+++ silk/collector.py
@@ -149,15 +149,13 @@ class DataCollector(with_metaclass(Singleton, object)):
             self.request.pyprofile = profile_text
 
             if SilkyConfig().SILKY_PYTHON_PROFILER_BINARY:
-                prefix = SilkyConfig().SILKY_PROF_FILE_PREFIX
-                file_name = self.request.prof_file.storage.get_available_name(
-                    prefix + "{}.prof".format(str(self.request.id))
-                )
+                file_path = self._get_profile_file_path()
                 content = ContentFile(marshal.dumps(ps.stats))
-                saved_name = self.request.prof_file.storage.save(file_name,
-                                                                 content)
-
-                self.request.prof_file = saved_name
+                saved_file_path = self.request.prof_file.storage.save(
+                    file_path,
+                    content
+                )
+                self.request.prof_file = saved_file_path
                 self.request.save()
 
         for _, query in self.queries.items():
@@ -189,6 +187,23 @@ class DataCollector(with_metaclass(Singleton, object)):
                 profile.queries = profile_query_models
                 profile.save()
         self._record_meta_profiling()
+
+    def _get_profile_file_path(self):
+        """Retrieve a valid file path for profile file
+
+        The file name will contain:
+        - prefix from SILKY_PROF_FILE_PREFIX settings
+        - the request path that we are profiling
+        - request id
+
+        """
+        request_path = self.request.path.replace('/', '_').lstrip('_')
+        proposed_path = '{prefix}{request_path}_{request_id}.prof'.format(
+            prefix=SilkyConfig().SILKY_PROF_FILE_PREFIX,
+            request_path=request_path,
+            request_id=str(self.request.id)
+        )
+        return self.request.prof_file.storage.get_available_name(proposed_path)
 
     def register_silk_query(self, *args):
         self.register_objects(TYP_SILK_QUERIES, *args)
