4697c8c05282eee985b1e0cef6649a06fe9c4bf4
Nasir Hussain <nasirhjafri@gmail.com>
2019-10-26 17:50:38 +0500

    Ref #369 Used .values instead of .items

--- silk/collector.py
+++ silk/collector.py
@@ -154,10 +154,11 @@ class DataCollector(with_metaclass(Singleton, object)):
                 self.request.prof_file = f.name
                 self.request.save()
 
-        for _, query in self.queries.items():
+        for query in self.queries.values():
             query_model = models.SQLQuery.objects.create(**query)
             query['model'] = query_model
-        for _, profile in self.profiles.items():
+
+        for profile in self.profiles.values():
             profile_query_models = []
             if TYP_QUERIES in profile:
                 profile_queries = profile[TYP_QUERIES]
