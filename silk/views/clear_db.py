fccaa0d55cd9b5caa242cf6806c85b998514540a
Nasir Hussain <nasirhjafri@gmail.com>
2020-01-29 16:33:58 +0500

    Used function to delete data from utils in views

--- silk/views/clear_db.py
+++ silk/views/clear_db.py
@@ -5,6 +5,7 @@ from django.views.generic import View
 
 from silk.auth import login_possibly_required, permissions_possibly_required
 from silk.models import Request, Response, SQLQuery, Profile
+from silk.utils.data_deletion import delete_model
 
 
 class ClearDBView(View):
@@ -27,7 +28,10 @@ class ClearDBView(View):
     def post(self, request, *_, **kwargs):
         context = {}
         if 'clear_all' in request.POST:
-            self._truncate_tables([Response, SQLQuery, Profile, Request])
+            delete_model(Profile)
+            delete_model(SQLQuery)
+            delete_model(Response)
+            delete_model(Request)
             tables = ['Response', 'SQLQuery', 'Profile', 'Request']
             context['msg'] = 'Cleared data for following silk tables: {0}'.format(', '.join(tables))
         return render(request, 'silk/clear_db.html', context=context)
