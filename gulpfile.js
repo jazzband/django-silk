6203fc707b5c27f8570bc776edd8bcc7008dbadf
Nasir Hussain <nasirhjafri@gmail.com>
2020-02-05 01:16:19 +0500

    Fixed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,58 @@
-var gulp = require('gulp'),
+let gulp = require('gulp'),
     async = require('async'),
     _ = require('underscore'),
-    plugins = require('gulp-load-plugins')();
+    plugins = require('gulp-load-plugins')(),
+    sass = require('gulp-sass');
 
 
-var webpack = {
-  externals: {
-    'react': 'React',
-    'jQuery': '$',
-    'underscore': '_',
-    'moment': 'moment',
-    'async': 'async'
-  },
-  devtool: 'inline-source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+let webpack = {
+    externals: {
+        'react': 'React',
+        'jQuery': '$',
+        'underscore': '_',
+        'moment': 'moment',
+        'async': 'async'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
 };
 
 
 gulp.task('watch', function () {
-  gulp.watch('scss/**/*.scss', ['scss']);
+    gulp.watch('scss/**/*.scss', gulp.series('scss'));
 });
 
-gulp.task('scss', function () {
-  gulp.src('scss/**/*.scss')
-      .pipe(plugins.scss())
-      .pipe(gulp.dest('silk/static/silk/css'))
+gulp.task('sass', function () {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('silk/static/silk/css'));
 });
 
-var apps = {
-  summary: 'js/summary/index.js'
+let apps = {
+    summary: 'js/summary/index.js'
 };
 
 gulp.task('js', function (done) {
-  var appNames = Object.keys(apps),
-      tasks = _.map(appNames, function (appName) {
-        return function (done) {
-          var path = apps[appName],
-              config = _.extend({}, webpack);
-          gulp.src(path)
-              .pipe(plugins.webpack(config))
-              .pipe(plugins.rename(appName + '.js'))
-              .pipe(gulp.dest('silk/static/silk/js'))
-              .on('end', done);
-        }
-      });
-  async.parallel(tasks, done);
+    let appNames = Object.keys(apps),
+        tasks = _.map(appNames, function (appName) {
+            return function (done) {
+                var path = apps[appName],
+                    config = _.extend({}, webpack);
+                gulp.src(path)
+                    .pipe(plugins.webpack(config))
+                    .pipe(plugins.rename(appName + '.js'))
+                    .pipe(gulp.dest('silk/static/silk/js'))
+                    .on('end', done);
+            }
+        });
+    async.parallel(tasks, done);
 });
 
