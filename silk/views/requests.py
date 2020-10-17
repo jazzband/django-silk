4465479d6ac5d1d54af99ce8e368e21efd5eaeee
Daniel Bradburn <daniel@crunchrapps.com>
2020-10-17 11:43:35 +0200

    fix filtering of requests

--- silk/views/requests.py
+++ silk/views/requests.py
@@ -6,7 +6,7 @@ from django.views.generic import View
 
 from silk.auth import login_possibly_required, permissions_possibly_required
 from silk.models import Request, Response
-from silk.request_filters import BaseFilter, filters_from_request
+from silk.request_filters import BaseFilter, filters_from_query_dict
 
 __author__ = 'mtford'
 
@@ -153,6 +153,6 @@ class RequestsView(View):
     @method_decorator(login_possibly_required)
     @method_decorator(permissions_possibly_required)
     def post(self, request):
-        filters = filters_from_request(request)
+        filters = filters_from_query_dict(request.POST)
         request.session[self.session_key_request_filters] = {ident: f.as_dict() for ident, f in filters.items()}
         return render(request, 'silk/requests.html', self._create_context(request))
