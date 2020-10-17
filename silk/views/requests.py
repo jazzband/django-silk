2d9ac63f68fc94cd8e0d8daa230fd066bf1d7e92
Daniel Bradburn <daniel@crunchrapps.com>
2020-10-17 14:45:19 +0200

    fixed filtering on requests page

--- silk/views/requests.py
+++ silk/views/requests.py
@@ -1,17 +1,13 @@
 from django.db.models import Sum
-from django.shortcuts import render
-from django.template.context_processors import csrf
-from django.utils.decorators import method_decorator
-from django.views.generic import View
-
-from silk.auth import login_possibly_required, permissions_possibly_required
 from silk.models import Request, Response
-from silk.request_filters import BaseFilter, filters_from_query_dict
+from silk.views.filterable_requests_view import FilterableRequestsView
+
 
 __author__ = 'mtford'
 
 
-class RequestsView(View):
+class RequestsView(FilterableRequestsView):
+    template = 'silk/requests.html'
     show = [5, 10, 25, 100, 250]
     default_show = 25
 
@@ -93,12 +89,9 @@ class RequestsView(View):
             'method'
         ).distinct()
 
-    def _get_objects(self, show=None, order_by=None, order_dir=None, path=None, filters=None):
-        if not filters:
-            filters = []
+    def _extend_queryset(self, query_set, show=None, order_by=None, order_dir=None):
         if not show:
             show = self.default_show
-        query_set = Request.objects.all()
         if not order_by:
             order_by = self.default_order_by
         if not order_dir:
@@ -109,11 +102,6 @@ class RequestsView(View):
         if ob['additional_query_filter'] is not None:
             query_set = ob['additional_query_filter'](query_set)
         query_set = query_set.order_by('%s%s' % ('-' if order_dir == 'DESC' else '', order_by))
-        if path:
-            query_set = query_set.filter(path=path)
-        for f in filters:
-            query_set = f.contribute_to_query_set(query_set)
-            query_set = query_set.filter(f)
         return query_set[:show]
 
     def _create_context(self, request):
@@ -122,37 +110,14 @@ class RequestsView(View):
         order_dir = request.GET.get('order_dir', self.default_order_dir)
         if show:
             show = int(show)
-        path = request.GET.get('path', None)
-        raw_filters = request.session.get(self.session_key_request_filters, {})
-        context = {
-            'show': show,
-            'order_by': order_by,
-            'order_dir': order_dir,
-            'request': request,
-            'options_show': self.show,
-            'options_order_by': self.options_order_by,
-            'options_order_dir': self.options_order_dir,
-            'options_paths': self._get_paths(),
-            'options_status_codes': self._get_status_codes(),
-            'options_methods': self._get_methods(),
-            'view_names': self._get_views(),
-            'filters': raw_filters
-        }
-        context.update(csrf(request))
-        if path:
-            context['path'] = path
-        context['results'] = self._get_objects(show, order_by, order_dir, path,
-                                               filters=[BaseFilter.from_dict(x) for _, x in raw_filters.items()])
+
+        context = super(RequestsView, self)._create_context(request)
+        context['show'] = show
+        context['order_by'] = order_by
+        context['order_dir'] = order_dir
+        context['options_show'] = self.show
+        context['options_order_by'] = self.options_order_by
+        context['options_order_dir'] = self.options_order_dir
+        context['results'] = self._extend_queryset(context['results'], show, order_by, order_dir)
+
         return context
-
-    @method_decorator(login_possibly_required)
-    @method_decorator(permissions_possibly_required)
-    def get(self, request):
-        return render(request, 'silk/requests.html', self._create_context(request))
-
-    @method_decorator(login_possibly_required)
-    @method_decorator(permissions_possibly_required)
-    def post(self, request):
-        filters = filters_from_query_dict(request.POST)
-        request.session[self.session_key_request_filters] = {ident: f.as_dict() for ident, f in filters.items()}
-        return render(request, 'silk/requests.html', self._create_context(request))
