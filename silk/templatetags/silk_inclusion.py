9619613a03aa52757d29f955491e39441f091719
Matt Di Florio <mattdiflorio@gmail.com>
2020-12-05 12:39:41 +0100

    Add row view for requests page (#440)
    * Add the styles for a row view in the requests page
    
    * Add the styles for a row view in the requests page
    
    * Remove useless comments
    
    * Add view style selector to the requests page.
    
    * Correct Gulp watch error.
    
    * Correct spacing for the numbers in the row view.
    
    * Change the name of request_row_summary.html to request_summary_row.html
    
    * Update dev environnement README instructions

--- silk/templatetags/silk_inclusion.py
+++ silk/templatetags/silk_inclusion.py
@@ -6,6 +6,8 @@ register = Library()
 def request_summary(silk_request):
     return {'silk_request': silk_request}
 
+def request_summary_row(silk_request):
+    return {'silk_request': silk_request}
 
 def request_menu(request, silk_request):
     return {'request': request,
@@ -36,6 +38,7 @@ def code(lines, actual_line):
 
 
 register.inclusion_tag('silk/inclusion/request_summary.html')(request_summary)
+register.inclusion_tag('silk/inclusion/request_summary_row.html')(request_summary_row)
 register.inclusion_tag('silk/inclusion/profile_summary.html')(profile_summary)
 register.inclusion_tag('silk/inclusion/code.html')(code)
 register.inclusion_tag('silk/inclusion/request_menu.html')(request_menu)
