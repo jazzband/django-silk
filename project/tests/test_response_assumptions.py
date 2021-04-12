a23686199bb339c16d3a88a6a02c3808a9d0203a
João Ricardo Lourenço <jorl17.8@gmail.com>
2021-04-12 23:58:44 +0100

    Add a simple test to make sure our assumptions about HttpResponse's headers interface holds true

---
+++ project/tests/test_response_assumptions.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.test import TestCase
+
+
+class TestResponseAssumptions(TestCase):
+
+    def test_headers_present_in_http_response(self):
+        """Verify that HttpResponse has a headers or _headers attribute, which we use and Mock in our tests."""
+        django_response = HttpResponse()
+        self.assertTrue(hasattr(django_response, '_headers') or hasattr(django_response, 'headers'))
