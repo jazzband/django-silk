11aa8cd8c6bb8eee46878e2e90da540c85c7bea3
marius-mather <61438033+marius-mather@users.noreply.github.com>
2021-09-21 22:02:02 +1000

    Add tests that access the actual DB (#493)
    * Add factory for example Blind model
    
    * Add tests that access the DB
    
    * Skip analyze when not supported
    
    * Fallback to prefix without analyze
    
    * Check for specific error message, rethrow if not matched
    
    Co-authored-by: warsquid <warsquid@protonmail.com>

---
+++ project/tests/test_db.py
@@ -0,0 +1,56 @@
+"""
+Test profiling of DB queries without mocking, to catch possible
+incompatibility
+"""
+from django.shortcuts import reverse
+from django.test import Client, TestCase
+from silk.collector import DataCollector
+from silk.config import SilkyConfig
+from silk.models import Request
+from silk.profiling.profiler import silk_profile
+
+from .factories import BlindFactory
+
+
+class TestDbQueries(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        objects = BlindFactory.create_batch(size=5)
+        SilkyConfig().SILKY_META = False
+
+    def test_profile_request_to_db(self):
+        client = Client()
+        DataCollector().configure(Request(reverse('example_app:index')))
+
+        with silk_profile(name='test_profile'):
+            resp = client.get(reverse('example_app:index'))
+
+        profile = list(DataCollector().profiles.values())[0]
+        assert len(resp.context['blinds']) == 5
+
+
+class TestAnalyzeQueries(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        objects = BlindFactory.create_batch(size=5)
+        SilkyConfig().SILKY_META = False
+        SilkyConfig().SILKY_ANALYZE_QUERIES = True
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        SilkyConfig().SILKLY_ANALYZE_QUERIES = False
+
+    def test_analyze_queries(self):
+        DataCollector().configure(Request(reverse('example_app:index')))
+        client = Client()
+
+        with silk_profile(name='test_profile'):
+            resp = client.get(reverse('example_app:index'))
+
+        profile = list(DataCollector().profiles.values())[0]
+        assert len(resp.context['blinds']) == 5
+
