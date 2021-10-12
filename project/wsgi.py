7c8fcd3c6d71a56a558f41c573fa250fd4d8714f
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-10-12 02:11:35 -0700

    noqa on wsgi application

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 import os
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
+from django.core.wsgi import get_wsgi_application # noqa: E402
 
 application = get_wsgi_application()
