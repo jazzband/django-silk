31c1356f597940927c08ec875e85d92812e6880f
Albert Wang <git@albertyw.com>
2022-03-19 19:09:15 -0400

    Clean up gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,5 @@
 let gulp = require('gulp'),
-    async = require('async'),
-    _ = require('underscore'),
-    plugins = require('gulp-load-plugins')(),
     sass = require('gulp-sass');
-
-
-let webpack = {
-    externals: {
-        'react': 'React',
-        'jQuery': '$',
-        'underscore': '_',
-        'moment': 'moment',
-        'async': 'async'
-    },
-    devtool: 'inline-source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
-        ]
-    }
-};
 
 
 gulp.task('watch', function () {
@@ -34,8 +10,4 @@ gulp.task('sass', function () {
     return gulp.src('scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('silk/static/silk/css'));
-});
-
-gulp.task('js', function (done) {
-    async.parallel([], done);
 });
