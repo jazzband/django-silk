689d47668b831b89c2fea698cabeb9e7c4630245
Albert Wang <git@albertyw.com>
2022-03-19 19:01:01 -0400

    Remove unused gulp js compilation

--- gulpfile.js
+++ gulpfile.js
@@ -36,22 +36,6 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('silk/static/silk/css'));
 });
 
-let apps = {
-    summary: 'js/summary/index.js'
-};
-
 gulp.task('js', function (done) {
-    let appNames = Object.keys(apps),
-        tasks = _.map(appNames, function (appName) {
-            return function (done) {
-                var path = apps[appName],
-                    config = _.extend({}, webpack);
-                gulp.src(path)
-                    .pipe(plugins.webpack(config))
-                    .pipe(plugins.rename(appName + '.js'))
-                    .pipe(gulp.dest('silk/static/silk/js'))
-                    .on('end', done);
-            }
-        });
-    async.parallel(tasks, done);
+    async.parallel([], done);
 });
