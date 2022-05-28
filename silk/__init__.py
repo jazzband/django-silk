62a19914a5c88d6c2151847d45843cd323101fb5
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2022-05-28 16:30:59 +0300

    Drop support for Django 2.2 (EOL)

--- silk/__init__.py
+++ silk/__init__.py
@@ -1,10 +1,6 @@
-import django
 from pkg_resources import DistributionNotFound, get_distribution
 
 try:
     __version__ = get_distribution("django-silk").version
 except DistributionNotFound:
     pass
-
-if django.VERSION < (3, 2):
-    default_app_config = "silk.apps.SilkAppConfig"
