0f5c994cf67c13fae1d53e2129303a2e53568a67
Albert Wang <git@albertyw.com>
2022-05-28 08:33:34 -0700

    Remove unused js compilation pipeline (#561)
    * Remove unused gulp js compilation
    
    * Clean up gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,5 @@
 let gulp = require('gulp'),
-    async = require('async'),
-    _ = require('underscore'),
-    plugins = require('gulp-load-plugins')(),
     sass = require('gulp-sass');
-
-
-let webpack = {
-    externals: {
-        'react': 'React',
-        'jQuery': '$',
-        'underscore': '_',
-        'moment': 'moment',
-        'async': 'async'
-    },
-    devtool: 'inline-source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
-        ]
-    }
-};
 
 
 gulp.task('watch', function () {
@@ -34,24 +10,4 @@ gulp.task('sass', function () {
     return gulp.src('scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('silk/static/silk/css'));
-});
-
-let apps = {
-    summary: 'js/summary/index.js'
-};
-
-gulp.task('js', function (done) {
-    let appNames = Object.keys(apps),
-        tasks = _.map(appNames, function (appName) {
-            return function (done) {
-                var path = apps[appName],
-                    config = _.extend({}, webpack);
-                gulp.src(path)
-                    .pipe(plugins.webpack(config))
-                    .pipe(plugins.rename(appName + '.js'))
-                    .pipe(gulp.dest('silk/static/silk/js'))
-                    .on('end', done);
-            }
-        });
-    async.parallel(tasks, done);
 });
