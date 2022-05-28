dcc05e6a83dbc193912435699f6aa6ab55f6cfb7
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2022-05-28 16:24:08 +0300

    Apply django-upgrade for django==2.2

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,9 +1,8 @@
 from django.conf import settings
-from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path(
