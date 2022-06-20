c59acbaa1a26057915db638903ae8b070434db0b
Nikolaus Schlemm <948118+nschlemm@users.noreply.github.com>
2022-06-20 09:34:18 +0200

    Added silk_request_garbage_collect command for out-of-band garbage collection. (#541)
    * Added silk_request_garbage_collect command for out-of-band garbage collection.
    
    To avoid deadlock issues as mentioned e.g. in #265 #294 #371
    Based on https://github.com/jazzband/django-silk/issues/265#issuecomment-705482767
    
    * Okay, to please @codecov :)
    
    Co-authored-by: Nikolaus Schlemm <capo@coder-nostra.de>
    Co-authored-by: Albert Wang <git@albertyw.com>

---
+++ project/tests/test_command_garbage_collect.py
@@ -0,0 +1,22 @@
+from django.core import management
+from django.test import TestCase
+
+from silk import models
+from silk.config import SilkyConfig
+
+from .factories import RequestMinFactory
+
+
+class TestViewClearDB(TestCase):
+    def test_garbage_collect_command(self):
+        SilkyConfig().SILKY_MAX_RECORDED_REQUESTS = 2
+        RequestMinFactory.create_batch(3)
+        self.assertEqual(models.Request.objects.count(), 3)
+        management.call_command("silk_request_garbage_collect")
+        self.assertEqual(models.Request.objects.count(), 2)
+        management.call_command("silk_request_garbage_collect", max_requests=1)
+        self.assertEqual(models.Request.objects.count(), 1)
+        management.call_command(
+            "silk_request_garbage_collect", max_requests=0, verbosity=2
+        )
+        self.assertEqual(models.Request.objects.count(), 0)
