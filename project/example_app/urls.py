e814d0c520165eb2cb0630a6f283b8ed0b803c6d
Sébastien Corbin <seb.corbin@gmail.com>
2022-10-29 22:28:21 +0200

    #594 Silk fails on constraint check queries (#618)

--- project/example_app/urls.py
+++ project/example_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'example_app'
 urlpatterns = [
     path(route='', view=views.index, name='index'),
+    path(route='create', view=views.ExampleCreateView.as_view(), name='create'),
 ]
