8583560e9d835884c57462cd29d6028f02c4da9b
Josh <josh@joshthomas.dev>
2022-10-29 15:09:46 -0500

    Add Python 3.11 to CI, tox, and trove classifiers (#612)
    * add python 3.11 to CI, tox, and trove classifiers
    
    * remove psycopg2-binary version restriction
    
    * add python 3.11 to list in README
    
    * python 3.11 only officially supported by Django 4.1+

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
