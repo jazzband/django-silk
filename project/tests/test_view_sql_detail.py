a54fd45a064fe7c0bff1f85b52c7b53c3b9e027a
Sébastien Corbin <seb.corbin@gmail.com>
2022-11-04 08:21:48 +0100

    Highlight non third-party code in traceback (#609)
    * Highlight non third-party code in traceback
    
    * Add tests

--- project/tests/test_view_sql_detail.py
+++ project/tests/test_view_sql_detail.py
@@ -1,4 +1,6 @@
+import os
 import random
+from unittest.mock import patch
 
 from django.conf import settings
 from django.test import TestCase
@@ -55,3 +57,25 @@ class TestViewSQLDetail(TestCase):
                                        'file_path': file_path
         })
         self.assertTrue(response.status_code == 403)
+
+    def test_virtualenv_not_available_no_highlight(self):
+        """if we don't have a virtualenv, there should be no code hightlighted"""
+        request = MockSuite().mock_request()
+        query = MockSuite().mock_sql_queries(request=request)[0]
+        url = silky_reverse('request_sql_detail', kwargs={'sql_id': query.id, 'request_id': request.id})
+        with patch.dict(os.environ, {}, clear=True):
+            self.assertIsNone(os.environ.get('VIRTUAL_ENV'))
+            response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, ' is-third-party')
+        self.assertNotContains(response, ' not-third-party')
+
+    def test_virtualenv_hightlight(self):
+        """if we have a virtualenv, there should be code hightlighted"""
+        request = MockSuite().mock_request()
+        query = MockSuite().mock_sql_queries(request=request)[0]
+        url = silky_reverse('request_sql_detail', kwargs={'sql_id': query.id, 'request_id': request.id})
+        with patch.dict(os.environ, {'VIRTUAL_ENV': '/some/virtualenv/that/doesnt/really/exist'}, clear=True):
+            response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, ' not-third-party')
