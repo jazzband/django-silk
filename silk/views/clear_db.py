131e608822fdfc46df43a3d1a79c8b1e93286dbc
Viral Joshi <viralj@users.noreply.github.com>
2023-04-06 13:01:58 -0500

    Update clear_db.py
    Added option to check for `SILKY_DELETE_PROFILES` set by user in settings file which if set true, all profiles will be deleted along with database clean function.

--- silk/views/clear_db.py
+++ silk/views/clear_db.py
@@ -1,3 +1,4 @@
+import os, shutil
 from django.db import transaction
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
@@ -6,6 +7,7 @@ from django.views.generic import View
 from silk.auth import login_possibly_required, permissions_possibly_required
 from silk.models import Profile, Request, Response, SQLQuery
 from silk.utils.data_deletion import delete_model
+from silk.config import SilkyConfig
 
 
 @method_decorator(transaction.non_atomic_requests, name="dispatch")
@@ -27,4 +29,15 @@ class ClearDBView(View):
             delete_model(Request)
             tables = ['Response', 'SQLQuery', 'Profile', 'Request']
             context['msg'] = 'Cleared data for following silk tables: {}'.format(', '.join(tables))
+
+            if SilkyConfig().SILKY_DELETE_PROFILES:
+                dir = SilkyConfig().SILKY_PYTHON_PROFILER_RESULT_PATH
+                for files in os.listdir(dir):
+                    path = os.path.join(dir, files)
+                    try:
+                        shutil.rmtree(path)
+                    except OSError:
+                        os.remove(path)
+                context['msg'] += '\nDeleted all profiles from the directory.'
+
         return render(request, 'silk/clear_db.html', context=context)
