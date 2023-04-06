757f253bdc514743e2721eb160a83e1c3eafc739
Viral Joshi <viralj@users.noreply.github.com>
2023-04-06 13:16:49 -0500

    Update test_view_clear_db.py
    Added new test that will test delete all profile functionality.

--- project/tests/test_view_clear_db.py
+++ project/tests/test_view_clear_db.py
@@ -20,3 +20,18 @@ class TestViewClearDB(TestCase):
         response = self.client.post(silky_reverse("cleardb"), {"clear_all": "on"})
         self.assertTrue(response.status_code == 200)
         self.assertEqual(models.Request.objects.count(), 0)
+        
+class TestViewClearDBAndDeleteProfiles(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        SilkyConfig().SILKY_AUTHENTICATION = False
+        SilkyConfig().SILKY_AUTHORISATION = False
+        SilkyConfig().SILKY_DELETE_PROFILES = True
+
+    def test_clear_all_and_delete_profiles(self):
+        RequestMinFactory.create()
+        self.assertEqual(models.Request.objects.count(), 1)
+        response = self.client.post(silky_reverse("cleardb"), {"clear_all": "on"})
+        self.assertTrue(response.status_code == 200)
+        self.assertEqual(models.Request.objects.count(), 0)
