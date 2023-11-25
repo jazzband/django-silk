64c02f838f675ef6822ce3108739b073ff13f5e4
Albert Wang <git@albertyw.com>
2023-11-25 18:40:00 -0500

    Support python 3.12 (#683)
    * Support python 3.12
    
    * Ensure setuptools is in the python3.12 environment

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
