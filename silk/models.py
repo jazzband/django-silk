2ce501aec625467713ac461f7909ad5d77444a2f
Mohamed Beltagy <beltagymohamed@gmail.com>
2024-07-09 12:02:12 +0200

    fix : bug use datetime and convert to milliseconds

--- silk/models.py
+++ silk/models.py
@@ -10,6 +10,7 @@ from django.db import models, transaction
 from django.db.models import (
     BooleanField,
     CharField,
+    DateField,
     DateTimeField,
     ExpressionWrapper,
     F,
@@ -130,16 +131,16 @@ class Request(models.Model):
         """"
         Calculate the total time spent on SQL queries using Django aggregates.
         """
-
-        total_time = SQLQuery.objects.filter(request=self).aggregate(
+        total_time_seconds = SQLQuery.objects.filter(request=self).aggregate(
             total_time=Sum(
                 ExpressionWrapper(
-                    F('end_time') - F('start_time'),
-                    output_field=FloatField()
+                    (F('end_time') - F('start_time')),
+                    output_field=DateField()
                 )
             )
         )['total_time']
-        return total_time or 0.0
+        return total_time_seconds.total_seconds() * 1000  if total_time_seconds is not None else 0.0
+        
 
     @property
     def headers(self):
