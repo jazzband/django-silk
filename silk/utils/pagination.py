760725c53a2648a2fa07ed98bb0f90e2a1e6ce71
Neal Striegler-Pettersson <neal.s.pettersson@d1g1t.com>
2025-10-31 19:22:58 -0400

    Handle invalid pages, add page size selector

--- silk/utils/pagination.py
+++ silk/utils/pagination.py
@@ -3,8 +3,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 __author__ = 'mtford'
 
 
-def _page(request, query_set):
-    per_page = request.GET.get('per_page', 200)
+def _page(request, query_set, per_page=200):
     paginator = Paginator(query_set, per_page)
     page_number = request.GET.get('page')
     try:
