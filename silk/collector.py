5c650fa68d97eb4490d0953d693969f16f8d36b6
Albert Wang <git@albertyw.com>
2026-03-06 21:22:40 -0800

    Fix test coverage issue

--- silk/collector.py
+++ silk/collector.py
@@ -95,7 +95,7 @@ class DataCollector(metaclass=Singleton):
             self.local.pythonprofiler = cProfile.Profile()
             try:
                 self.local.pythonprofiler.enable()
-            except ValueError as e:
+            except ValueError as e:  # pragma: no cover
                 # Deal with cProfile not being allowed to run concurrently
                 # https://github.com/jazzband/django-silk/issues/682
                 Logger.error('Could not enable python profiler, %s' % str(e), exc_info=True)
