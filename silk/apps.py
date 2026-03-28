86b6c0faa90952ac76216359a52a329c7de7ec14
Johnny <93166599+johnny-official@users.noreply.github.com>
2026-03-28 11:35:44 +0700

    refactor: add runtime django version check
    Signed-off-by: Johnny <93166599+johnny-official@users.noreply.github.com>

--- silk/apps.py
+++ silk/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+import django
+from django.core.exceptions import ImproperlyConfigured
 
 
 class SilkAppConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = "silk"
+
+    def ready(self):
+        if django.VERSION < (4, 2):
+            raise ImproperlyConfigured(
+                'Django Silk requires Django 4.2 or later. '
+                'Please upgrade your Django version.'
+            )
