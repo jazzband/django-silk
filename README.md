22dd8b672f70bfec807c55ef403d0dd154c3cdc7
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2026-07-31 19:11:20 +0300

    Drop support for EOL Django 5.1

--- README.md
+++ README.md
@@ -30,8 +30,9 @@ Silk is a live profiling and inspection tool for the Django framework. Silk inte
 
 Silk has been tested with:
 
-* Django: 5.1, 5.2, 6.0
+* Django: 5.2, 6.0
 * Python: 3.10, 3.11, 3.12, 3.13, 3.14, 3.15
+
 ## Installation
 
 Via pip into a `virtualenv`:
--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 5.1',
         'Framework :: Django :: 5.2',
         'Framework :: Django :: 6.0',
         'Intended Audience :: Developers',
@@ -37,7 +36,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        'Django>=5.1',
+        'Django>=5.2',
         'sqlparse',
         'gprof2dot>=2017.09.19',
     ],
