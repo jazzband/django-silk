0d77ede0746e2dfd840fb01d7fbb7bbf6d1fce89
Albert Wang <git@albertyw.com>
2026-08-01 22:04:43 -0700

    Assert only the HttpResponse.headers attribute that Django 4.2+ actually has

--- project/tests/test_response_assumptions.py
+++ project/tests/test_response_assumptions.py
@@ -5,6 +5,6 @@ from django.test import TestCase
 class TestResponseAssumptions(TestCase):
 
     def test_headers_present_in_http_response(self):
-        """Verify that HttpResponse has a headers or _headers attribute, which we use and Mock in our tests."""
+        """Verify that HttpResponse has a headers attribute, which ResponseModelFactory relies on."""
         django_response = HttpResponse()
-        self.assertTrue(hasattr(django_response, '_headers') or hasattr(django_response, 'headers'))
+        self.assertTrue(hasattr(django_response, 'headers'))
